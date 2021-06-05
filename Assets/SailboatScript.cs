bb7804cbab07377c7917d691d35d76918521e33e
Erik Högberg <erik.hogberg.9945@student.uu.se>
2021-06-05 15:46:12 +0200

    init with planet model

---
+++ Assets/SailboatScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SailboatScript : MonoBehaviour {
+
+	public GameObject Rudder;
+	public GameObject Mainsail;
+	public GameObject Foresail;
+
+	void Start() {
+
+	}
+
+	void Update() {
+
+	}
+}
